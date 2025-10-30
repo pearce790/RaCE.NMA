5f0cb81597d6299a2df9b498260d61653265c196
Michael Pearce <pearce790@gmail.com>
2025-10-30 09:37:01 -0700

    include reproducibility vignette in menu bar

---
+++ R/WangPosteriors.R
@@ -0,0 +1,8 @@
+#' Posterior relative treatment effects from a network meta-analysis by Wang et al. (2022) of 11 frontline immunochemotherapies for follicular lymphoma
+#'
+#' This real data set includes the posterior draws of relative treatment effects based on a network meta-analysis
+#' of 1 frontline immunochemotherapies for follicular lymphoma.
+#'
+#' @format A data.frame with 60000 rows and 13 columns. Columns 4--13 are of primary use: They represent the posterior relative treatment effects of the 10 non-baseline treatments.
+#' @source {Originally produced in: Y Wang, S Zhou, X Qi, et al. Efficacy of front-line immunochemotherapy for follicular lymphoma: A network meta-analysis of randomized controlled trials. Blood Cancer Journal. 2022; 12(1):1.}
+"WangPosteriors"
