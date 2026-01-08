29174313eb690347f9d28434ead1e8301f8b85f5
Michael Pearce <pearce790@gmail.com>
2026-01-08 13:28:47 -0800

    adding toy dataset

---
+++ R/toy_data.R
@@ -0,0 +1,7 @@
+#' Toy data of posterior relative treatment effects
+#'
+#' The dataset includes relative treatment effects for 4 treatments across 10k posterior
+#' draws, as studied in Simulation Study 2 of Pearce and Zhou (2025).
+#'
+#' @format data.frame in which the (i,j) entry is the relative treatment effect of treatment j in posterior draw i.
+"toy_data"
