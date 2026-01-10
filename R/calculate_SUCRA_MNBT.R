fdcc18efcfedb5c8da8d247c56f026dc46e1c32d
Michael Pearce <pearce790@gmail.com>
2026-01-10 10:01:42 -0800

    minor vignette updates and changing "confidence" parameter to more accurate "credibile" in table function

--- R/calculate_SUCRA_MNBT.R
+++ R/calculate_SUCRA_MNBT.R
@@ -7,7 +7,7 @@
 #'
 #' @param data A NxJ matrix of data, where N is the number of observations and J the number of treatments. This feature is designed to display results from a standard NMA study.
 #' @param mcmc MCMC draws from the RaCE NMA model, in the form of the model output of the \code{mcmc_RCMVN} function.
-#' @param confidence The \code{confidence} parameter from the \code{gelman.diag} function in the \code{coda} package. Defaults to 0.95.
+#' @param credible The \code{credible} level for the interval estimate of MNBT. Defaults to 0.50.
 #' @param names A vector of intervention names (optional)
 #' @return A table containing SUCRA and MNBT values for each treatment, ordered by descending SUCRA values.
 #'
@@ -15,7 +15,7 @@
 #' mcmc <- mcmc_RCMVN(ybar=c(0,0,1,1), s=c(.1,.1,.1,.1), mu0=0.5, sigma0=5, tau=0.5,chains=2,seed=1)
 #' calculate_SUCRA_MNBT(mcmc=mcmc)
 #' @export
-calculate_SUCRA_MNBT <- function(data=NULL,mcmc=NULL,confidence=0.50,names=NULL){
+calculate_SUCRA_MNBT <- function(data=NULL,mcmc=NULL,credible=0.50,names=NULL){
   if(!is.null(data)){
     J <- ncol(data)
     if(is.null(names)){
@@ -28,12 +28,12 @@ calculate_SUCRA_MNBT <- function(data=NULL,mcmc=NULL,confidence=0.50,names=NULL)
 
     SUCRA = apply(data_ranks_probs_cumulative[1:(J-1),],2,mean)
     MNBT = apply(data_ranks,2,function(ranks){
-      values <- quantile(ranks-1,c(0.5,(1-confidence)/2,1-(1-confidence)/2))
+      values <- quantile(ranks-1,c(0.5,(1-credible)/2,1-(1-credible)/2))
       paste0(values[1]," (",values[2],", ",values[3],")")
     })
     result <- data.frame(Treatment=names,SUCRA=SUCRA,MNBT=MNBT) %>% arrange(desc(SUCRA),row.names=FALSE)
     rownames(result) <- NULL
-    names(result)[3] <- paste0("MNBT (",confidence*100,"% CI)")
+    names(result)[3] <- paste0("MNBT (",credible*100,"% CI)")
   }
   if(!is.null(mcmc)){
     mcmc_mu <- mcmc[,grep("mu",names(mcmc))]
@@ -48,12 +48,12 @@ calculate_SUCRA_MNBT <- function(data=NULL,mcmc=NULL,confidence=0.50,names=NULL)
 
     SUCRA = apply(mcmc_ranks_probs_cumulative[1:(J-1),],2,mean)
     MNBT = apply(mcmc_ranks,2,function(ranks){
-      values <- quantile(ranks-1,c(0.5,(1-confidence)/2,1-(1-confidence)/2))
+      values <- quantile(ranks-1,c(0.5,(1-credible)/2,1-(1-credible)/2))
       paste0(values[1]," (",values[2],", ",values[3],")")
     })
     result <- data.frame(Treatment=names,SUCRA=SUCRA,MNBT=MNBT) %>% arrange(desc(SUCRA),row.names=FALSE)
     rownames(result) <- NULL
-    names(result)[3] <- paste0("MNBT (",confidence*100,"% CI)")
+    names(result)[3] <- paste0("MNBT (",credible*100,"% CI)")
   }
   return(result)
 }
